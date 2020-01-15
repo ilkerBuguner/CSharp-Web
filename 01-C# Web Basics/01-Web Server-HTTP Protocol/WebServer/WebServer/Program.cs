7941e320f429bf6e2257a136c7d8e58d41e01d86
İlker Bugüner <ilker.buguner@gmail.com>
2020-01-15 12:53:34 +0200

    Made simple Web Server.

--- 01-C# Web Basics/01-Web Server-HTTP Protocol/WebServer/WebServer/Program.cs
+++ 01-C# Web Basics/01-Web Server-HTTP Protocol/WebServer/WebServer/Program.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebServer
 {
@@ -22,11 +24,18 @@ namespace WebServer
                 int bytesRead = networkStream.Read(requestedBytes, 0, requestedBytes.Length);
                 string request = Encoding.UTF8.GetString(requestedBytes, 0, bytesRead);
 
+                string responseText = @"<form action='/Account/Login' method='post'>
+<input type=date name='date' />
+<input type=text name='username' />
+<input type=password name='pasword' />
+<input type=submit value='Login' />
+</form>";
+
                 string response = "HTTP/1.0 200 OK" + NewLine +
                                   "Server: SoftuniServer/1.0" + NewLine +
                                   "Content-Type: text/html" + NewLine +
                                   NewLine +
-                                  "<h1>Hello<h1>";
+                                  responseText;
                 byte[] responseBytes = Encoding.UTF8.GetBytes(response);
                 networkStream.Write(responseBytes, 0, responseBytes.Length);
 
