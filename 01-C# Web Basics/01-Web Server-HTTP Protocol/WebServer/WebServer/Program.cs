1dab130473fc084087c7e593d46c4b50e34939b6
İlker Bugüner <ilker.buguner@gmail.com>
2020-01-19 14:28:52 +0200

    Modified the Web Server to run asynchronously and can accept multiple clients at the same time.

--- 01-C# Web Basics/01-Web Server-HTTP Protocol/WebServer/WebServer/Program.cs
+++ 01-C# Web Basics/01-Web Server-HTTP Protocol/WebServer/WebServer/Program.cs
@@ -9,40 +9,56 @@ namespace WebServer
 {
     class Program
     {
-        static void Main(string[] args)
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        static async Task Main(string[] args)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
-            const string NewLine = "\r\n";
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
 
             while (true)
             {
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Task.Run(async () =>
+                {
+                    TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    await ProcessClientAsync(tcpClient);
+                });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-                using NetworkStream networkStream = tcpClient.GetStream();
-                byte[] requestedBytes = new byte[1000000];
-                int bytesRead = networkStream.Read(requestedBytes, 0, requestedBytes.Length);
-                string request = Encoding.UTF8.GetString(requestedBytes, 0, bytesRead);
+            }
+        }
 
-                string responseText = @"<form action='/Account/Login' method='post'>
+        private static async Task
+            ProcessClientAsync(TcpClient tcpClient)
+        {
+            const string NewLine = "\r\n";
+
+            using NetworkStream networkStream = tcpClient.GetStream();
+            byte[] requestedBytes = new byte[1000000];
+            int bytesRead = await networkStream.ReadAsync(requestedBytes, 0, requestedBytes.Length);
+            string request = Encoding.UTF8.GetString(requestedBytes, 0, bytesRead);
+
+            string responseText = @"<form action='/Account/Login' method='post'>
 <input type=date name='date' />
 <input type=text name='username' />
 <input type=password name='pasword' />
 <input type=submit value='Login' />
 </form>";
 
-                string response = "HTTP/1.0 200 OK" + NewLine +
-                                  "Server: SoftuniServer/1.0" + NewLine +
-                                  "Content-Type: text/html" + NewLine +
-                                  NewLine +
-                                  responseText;
-                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                networkStream.Write(responseBytes, 0, responseBytes.Length);
+            string response = "HTTP/1.0 200 OK" + NewLine +
+                              "Server: SoftuniServer/1.0" + NewLine +
+                              "Content-Type: text/html" + NewLine +
+                              NewLine +
+                              responseText;
 
-                Console.WriteLine(request);
-                Console.WriteLine(new string('=', 60));
 
-            }
+            response = response + NewLine + DateTime.UtcNow.ToString();
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+            Console.WriteLine(request);
+            Console.WriteLine(new string('=', 60));
         }
     }
 }
